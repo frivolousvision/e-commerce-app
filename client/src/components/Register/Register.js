75aef2f5d460ab8c315bd26bd41f35b89b992a78
frivolousvision <frivolousvision@gmail.com>
2021-07-29 20:21:02 -0400

    resized forms implemented flexbox

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -19,7 +19,7 @@ const Register = ({ setAuth }) => {
     e.preventDefault();
     try {
       const body = { email, password, name };
-      const response = await fetch("http://localhost:5000/auth/register", {
+      const response = await fetch("/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
@@ -40,7 +40,7 @@ const Register = ({ setAuth }) => {
   };
   return (
     <Fragment>
-      <div className='login-container'>
+      <div className='register-container'>
         <h1 className='login-header'>Welcome, sign up here!</h1>
         <div className='form-container'>
           <form onSubmit={onSubmitForm}>
